be449eb6598dc6411e16c099320b485c96c77489
Pedro Henrique Munhoz Costa <pedromunhozc@gmail.com>
2023-01-22 17:07:10 -0300

    [Frontend Main Screen Implementation]
    - Creates new components for Header and Summary Table to be displayed
    on main screen.
    - Creates a support method to get all days from the beginning of
    current year to be used to display the day boxes on screen.
    - Configures the App to display the newly created components.
    
    Signed-off-by: Pedro Henrique Munhoz Costa <pedromunhozc@gmail.com>

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -1,21 +1,17 @@
+import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
 import './styles/globals.css';
 
-import { Habit } from "./components/Habits"
+// import { Habit } from "./components/Habits"
 
 
-function App() {
+export function App() {
   return (
-    <div>
-    <Habit completed={3} />
-    <Habit completed={5} />
-    <Habit completed={7} />
-    <Habit completed={9} />
-    <Habit completed={11} />
-    <Habit completed={13} />
-    <Habit completed={15} />
-    <Habit completed={17} />
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   )
 }
-
-export default App
